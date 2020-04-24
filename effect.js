6b3032b0547b9a4e1c5d3669fda0ccd1d097bba0
Kevin Cabrera <kevin.cabrera@mosedcorporation.com>
2020-04-24 17:26:02 +0800

    Update

--- effect.js
+++ effect.js
@@ -153,10 +153,10 @@ $('document').ready(function(){
 		$('#b11').animate({top:240, left: vw-350},500);
 		$('#b22').animate({top:240, left: vw-250},500);
 		$('#b33').animate({top:240, left: vw-150},500);
-		$('#b44').animate({top:440, left: vw-50},500);
-		$('#b55').animate({top:440, left: vw+50},500);
-		$('#b66').animate({top:440, left: vw+150},500);
-		$('#b77').animate({top:440, left: vw+250},500);
+		$('#b44').animate({top:240, left: vw-50},500);
+		$('#b55').animate({top:240, left: vw+50},500);
+		$('#b66').animate({top:240, left: vw+150},500);
+		$('#b77').animate({top:240, left: vw+250},500);
 		$('.balloons').css('opacity','0.9');
 		$('.balloons h2').fadeIn(3000);
 		$(this).fadeOut('slow').delay(3000).promise().done(function(){
